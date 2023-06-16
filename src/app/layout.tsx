c7f9602cb088c23a018b4d0bafd79408b2ffe7b3
Lucas-Winicius <lucasclucas7@gmail.com>
2023-06-15 21:30:18 -0300

    styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-import './globals.css'
+import StyledComponentsRegistry from '../lib/registry'
+import '@/styles/globals.css'
+
 export const metadata = {
   title: 'Portfolio',
   description: '',
 }
-
+ 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html>
       <body>
-        {children}
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
   )
