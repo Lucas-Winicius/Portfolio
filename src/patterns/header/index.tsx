d84555cd69911dfd19569257165752bdc8e8ecbf
Lucas-Winicius <lucasclucas7@gmail.com>
2023-06-23 23:59:41 -0300

    Change language update

--- src/patterns/header/index.tsx
+++ src/patterns/header/index.tsx
@@ -38,6 +38,7 @@ export default function Header() {
           </li>
           <li>
             <button onClick={toggleLang} title={t("header.button.title")}>
+              <span id="blur">{t("header.button.flag")}</span>
               <span>{t("header.button.flag")}</span>
             </button>
           </li>
