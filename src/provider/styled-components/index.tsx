074fd71d39866ca31aec2cef8dc5f31815956351
Lucas-Winicius <lucasclucas7@gmail.com>
2023-06-30 18:31:10 -0300

    style update

--- src/provider/styled-components/index.tsx
+++ src/provider/styled-components/index.tsx
@@ -2,15 +2,14 @@ import theme from "@/assets/css/theme";
 import StyledComponentsRegistry from "@/lib/registry";
 import { ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from "../../assets/css/global";
+// import GlobalStyles from "../../assets/css/global";
+import "../../assets/css/global.css";
 
 export const StyledComponents = ({ children }: { children: ReactNode }) => {
   return (
     <StyledComponentsRegistry>
-      <ThemeProvider theme={theme}>
-        {children}
-        <GlobalStyles />
-      </ThemeProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </StyledComponentsRegistry>
   );
 };
+// <GlobalStyles />
