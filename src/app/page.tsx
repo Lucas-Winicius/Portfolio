b32196dcee33da601c814ef263ce221205f19340
Lucas-Winicius <lucasclucas7@gmail.com>
2023-07-04 22:45:13 -0300

    Cookie notice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 import Landing from "@/patterns/landing/index";
 import Cookie from "@/patterns/cookie/index";
-import useCookieStore from "@/store/cookies";
 
 export default function Home() {
-  const cookies = useCookieStore((state) => state.cookie);
-
   return (
     <>
       <Landing />
-      {!cookies.cookie && <Cookie />}
+      <Cookie />
     </>
   );
 }
--- src/patterns/cookie/index.tsx
+++ src/patterns/cookie/index.tsx
@@ -1,25 +1,41 @@
 "use client";
 import React from "react";
-import useCookieStore from "@/store/cookies";
-
 import * as C from "./styled";
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
 
 function Cookie() {
-  const setCookie = useCookieStore((state) => state.setCookie);
+  const cookies = Cookies.get();
+  const [hasCookies, setHasCookies] = useState(true);
 
-  const acceptCookie = () => {
-    setCookie({ cookie: "accepted" });
+  useEffect(() => {
+    setHasCookies(Object.keys(cookies).length > 0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleClick = (): void => {
+    Cookies.set("lang", "ptBR", {
+      expires: 7,
+      sameSite: "strict",
+      path: "/"
+    });
+
+    setHasCookies(true);
   };
 
   return (
-    <C.Container>
-      <h1>Cookies!</h1>
-      <p>
-        Nós usamos cookies para melhorar sua experiência em nosso site. Ao
-        continuar navegando, você concorda com o uso de cookies.
-      </p>
-      <button onClick={acceptCookie}>Entendo</button>
-    </C.Container>
+    <>
+      {!hasCookies && (
+        <C.Container>
+          <h1>Cookies!</h1>
+          <p>
+            Nós usamos cookies para melhorar sua experiência em nosso site. Ao
+            continuar navegando, você concorda com o uso de cookies.
+          </p>
+          <button onClick={handleClick}>Entendi</button>
+        </C.Container>
+      )}
+    </>
   );
 }
 
