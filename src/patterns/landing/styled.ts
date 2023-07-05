2ba78aac70d049544e09c9036386e01b08ee7bba
Lucas-Winicius <lucasclucas7@gmail.com>
2023-07-05 17:08:53 -0300

    style

--- src/patterns/landing/styled.ts
+++ src/patterns/landing/styled.ts
@@ -15,7 +15,7 @@ export const primary = styled.div`
   p {
     color: white;
     width: 100%;
-    font-size: 30px;
+    font-size: 25px;
     font-weight: 500;
     text-align: center;
   }
@@ -29,7 +29,7 @@ export const primary = styled.div`
     -webkit-background-clip: text;
     color: transparent;
     background-image: linear-gradient(to right, #f02d3a, #b496e0);
-    font-size: 65px;
+    font-size: 55px;
     font-weight: 600;
   }
 `;
@@ -53,12 +53,21 @@ export const arrow = styled.div`
     animation: arrow infinite 3s;
   }
 
+  @keyframes text {
+    0% {
+      color: #fffa;
+    }
+    50% {
+      color: transparent;
+    }
+  }
+
   @keyframes arrow {
     0% {
       transform: translateY(0px) scale(0.7);
     }
     50% {
-      transform: translateY(25px) scale(0.7);
+      transform: translateY(20px) scale(0.7);
     }
   }
 `;
