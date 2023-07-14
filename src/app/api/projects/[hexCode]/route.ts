4eafe9f5d9f8339257df4f6bb6bf6864c614009a
Lucas-Winicius <lucasclucas7@gmail.com>
2023-07-14 13:09:45 -0300

    project - PUT

--- src/app/api/projects/[hexCode]/route.ts
+++ src/app/api/projects/[hexCode]/route.ts
@@ -19,3 +19,44 @@ export async function GET(request: NextRequest, context: any) {
     return NextResponse.json(projects, { status: 200 });
   }
 }
+
+export async function PUT(request: NextRequest, context: any) {
+  const hexCode = String(context?.params?.hexCode) || "";
+  const json = await request.json();
+
+  if (
+    !json.name ||
+    !json.description ||
+    !json.images ||
+    !json.github ||
+    !json.url ||
+    !json.technologies
+  ) {
+    return NextResponse.json(
+      {
+        status: 400,
+        message:
+          "Either one of the following parameters is missing: ( name | description | images | github | url | image )."
+      },
+      { status: 400 }
+    );
+  }
+
+  const projectEdit = await prisma.projects.update({
+    where: {
+      hexCode
+    },
+    data: {
+      name: json.name,
+      description: json.description,
+      images: json.images,
+      githubLink: json.github,
+      projectLink: json.url,
+      hexCode: hexCode,
+      visibility: json.visibility || "VISIBLE",
+      technologies: json.technologies
+    }
+  });
+
+  return NextResponse.json(projectEdit, { status: 200 });
+}
