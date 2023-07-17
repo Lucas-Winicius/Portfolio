33b22bb35a8017467bf7eba0361d30fc871bb7bf
Lucas-Winicius <lucasclucas7@gmail.com>
2023-07-17 18:56:34 -0300

    technology - PUT

--- src/app/api/technologies/route.ts
+++ src/app/api/technologies/route.ts
@@ -65,3 +65,41 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ status: 500, error: e }, { status: 500 });
   }
 }
+
+export async function PUT(request: Request) {
+  const json = await request.json();
+
+  const requiredKeys = [
+    "id",
+    "name",
+    "image"
+  ];
+
+  const missingKeys = checkkeys(requiredKeys, json);
+
+  if (missingKeys.length > 0) {
+    return NextResponse.json(
+      {
+        status: 400,
+        message: `You have missed the following parameters: ${missingKeys}`,
+      },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const technology = await prisma.technology.update({
+      where: {
+        id: json.id
+      },
+      data: {
+        name: json.name,
+        image: json.image,
+      },
+    });
+
+    return NextResponse.json(technology, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ status: 500, error: e }, { status: 500 });
+  }
+}
