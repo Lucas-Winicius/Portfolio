607988e3a8ea3bf27b495a919ce5ab96338f7c85
Lucas-Winicius <lucasclucas7@gmail.com>
2023-07-17 22:57:27 -0300

    get post

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -31,3 +31,36 @@ export async function POST(request: Request) {
     return NextResponse.json({ status: 500, error: e }, { status: 500 });
   }
 }
+
+export async function GET(request: NextRequest) {
+  const id = request.nextUrl.searchParams.get("id") || "";
+  const mode = request.nextUrl.searchParams.get("mode");
+
+  if (mode === "single") {
+    try {
+      const post = await prisma.post.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!post) {
+        return NextResponse.json(
+          { status: 404, message: "Post not found" },
+          { status: 404 }
+        );
+      }
+
+      return NextResponse.json(post, { status: 200 });
+    } catch (e) {
+      return NextResponse.json({ status: 500, error: e }, { status: 500 });
+    }
+  }
+
+  try {
+    const post = await prisma.post.findMany();
+    return NextResponse.json(post, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ status: 500, error: e }, { status: 500 });
+  }
+}
