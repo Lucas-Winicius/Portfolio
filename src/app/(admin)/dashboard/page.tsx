8e6df978c4ea2f2736ea15640407186867be8140
Lucas-Winicius <lucasclucas7@gmail.com>
2023-07-29 13:32:28 -0300

    dashbord

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,13 +1,4 @@
-"use client"
-import { redirect } from 'next/navigation';
-import { hasCookie } from 'cookies-next';
-
 export default function Dashboard() {
-
-  if(!hasCookie(`UserToken-${process.env.USER_TOKEN_CODE}`)) {
-    redirect("/login")
-  }
-
   return (
     <>
       <h1>Painel administrativo</h1>
