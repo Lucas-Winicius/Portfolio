5a86270a7f1a1d1a85ef99fd414e85c9e177ccb7
Lucas-Winicius <lucasclucas7@gmail.com>
2023-08-02 11:34:17 -0300

    Empty message update

--- src/components/EmptyDashboard.tsx
+++ src/components/EmptyDashboard.tsx
@@ -3,7 +3,7 @@ const EmptyDashboard = () => {
     <div className="w-full h-full flex justify-center items-center">
       <div className="-mt-20 space-y-8 select-none mx-5">
         <h1 className="text-7xl text-center text-zinc-100 ">-_-</h1>
-        <p className="text-zinc-100 text-center">Tão vazio. Tente selecionar alguma operação ou selecione outra.</p>
+        <p className="text-zinc-100 text-center">Tão vazio. Tente selecionar alguma operação.</p>
       </div>
     </div>
   );
