f00be3084238af3604a963e6051fa76be2732686
Lucas-Winicius <lucasclucas7@gmail.com>
2023-08-12 22:44:49 -0300

    Update in the display of technologies.

--- src/patterns/DashboardManager.tsx
+++ src/patterns/DashboardManager.tsx
@@ -2,13 +2,14 @@ import EmptyDashboard from "@/components/EmptyDashboard";
 import { useSearchParams } from "next/navigation";
 import ViewProjects from "./ViewProjects";
 import ViewPosts from "./ViewPosts";
+import ViewTecs from "./ViewTecs";
 
 function DashboardManager() {
   const mode = useSearchParams().get("mode");
 
   if (mode == "projects") return <ViewProjects />;
   if (mode == "posts") return <ViewPosts />;
-  if (mode == "technologies") return <h1>TECNOLOGIAS</h1>;
+  if (mode == "technologies") return <ViewTecs/>;
 
   return <EmptyDashboard/>
 }
---
+++ src/patterns/ViewTecs.tsx
@@ -0,0 +1,10 @@
+function ViewTecs() {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center space-y-5 select-none">
+      <h1 className="text-5xl font-extrabold">Página indisponível</h1>
+      <p className="text-sm">No momento não é possível acessar a seção de tecnologias.</p>
+    </div>
+  );
+}
+
+export default ViewTecs;
