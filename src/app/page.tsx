f6b373bac9e203a72937941a3d32eea7b686a7c5
Lucas-Winicius <lucasclucas7@gmail.com>
2023-08-17 10:04:44 -0300

    cookie notification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import ArrowToTop from "@/components/ArrowToTop"
-import About from "@/patterns/About"
-import Landing from "@/patterns/Landing"
-import Projects from "@/patterns/Projects"
+import ArrowToTop from "@/components/ArrowToTop";
+import Cookies from "@/components/Cookies";
+import About from "@/patterns/About";
+import Landing from "@/patterns/Landing";
+import Projects from "@/patterns/Projects";
 
 export default function Home() {
   return (
     <>
       <Landing />
-      <ArrowToTop />
       <About />
       <Projects />
+      <ArrowToTop />
+      <Cookies />
     </>
-  )
+  );
 }
---
+++ src/components/Cookies.tsx
@@ -0,0 +1,42 @@
+"use client";
+import jsCookies from "js-cookie";
+import { useEffect, useState } from "react";
+
+function Cookies() {
+  const [hide, setHide] = useState(true);
+
+  useEffect(() => {
+    setHide(!!jsCookies.get().CookieConfirmation);
+  }, []);
+
+  const handleConfirmation = () => {
+    jsCookies.set("CookieConfirmation", "accepted", {
+      path: "/",
+      expires: 15,
+      sameSite: "Lax",
+    });
+    setHide(true);
+  };
+
+  return (
+    <div
+      className={`fixed left-4 bottom-4 w-1/4 bg-slate-100 rounded p-2 text-zinc-950 font-bold space-y-2 ${
+        hide && "hidden"
+      }`}
+    >
+      <p>
+        Utilizamos cookies com o objetivo de melhorar a sua experiência como
+        usuário. Ao utilizar a página, você concorda que nós utilizemos cookies.
+        🍪
+      </p>
+      <button
+        onClick={handleConfirmation}
+        className="bg-zinc-950 w-full text-slate-50 py-2"
+      >
+        Entendo
+      </button>
+    </div>
+  );
+}
+
+export default Cookies;
