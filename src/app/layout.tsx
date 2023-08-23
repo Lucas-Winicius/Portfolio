c220786163e82f02ad1316ae0b7ba3de865f6731
Lucas-Winicius <lucasclucas7@gmail.com>
2023-08-23 10:44:02 -0300

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/patterns/Header";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Lucas Winicius',
-    default: 'Lucas Winicius',
+    template: "%s - Lucas Winicius",
+    default: "Lucas Winicius",
   },
   description: "",
 };
@@ -23,6 +24,16 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=UA-228198436-2"></Script>
+        <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-228198436-2');`
+        }
+        </Script>
       </body>
     </html>
   );
